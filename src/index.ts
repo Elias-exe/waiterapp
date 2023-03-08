20f6f1d43953c161e9008f70a39d26895076e9b9
Elias-Exe <adventurergamer11@hotmail.com>
2023-03-07 21:39:08 -0300

    feat: First Commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017')
+    .then(() => {
+        const app = express();
+        app.listen(3001, () => {
+            console.log('🌹 Server is running on http://localhost:3001');
+        });
+    } )
+    .catch(() => console.log('erro'));
+
+
